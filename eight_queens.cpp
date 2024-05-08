16a93331cf4ba781bdb71c777d63653856cb8ca4
emanuks <emanuels313@gmail.com>
2024-05-08 16:51:37 -0300

    feat: adding invalid board implementation

--- eight_queens.cpp
+++ eight_queens.cpp
@@ -12,5 +12,18 @@
 #include <string>
 
 int is_valid_eight_queens(std::string file_name) {
+    std::ifstream file;
+    bool board[8][8] = {false};
+    file.open(file_name);
 
+    if (file.is_open()) {
+        std::string line;
+        while (file) {
+            std::getline(file, line);
+            if (line.length() != 8) return -1;
+        }
+    }
+
+    file.close();
+    return 0;
 }
