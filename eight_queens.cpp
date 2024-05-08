6dbe09ddd3c5f8ac064b478fbfb2d51bf888b322
emanuks <emanuels313@gmail.com>
2024-05-08 18:49:29 -0300

    feat: adding output file logic

--- eight_queens.cpp
+++ eight_queens.cpp
@@ -13,24 +13,36 @@
 
 #include "./eight_queens.hpp"
 
-bool check_if_threathens_other_queen(int x, int y, bool board[8][8]) {
+std::string format_collision(int x, int y, int i, int j) {
+    return std::to_string(x) + "," +
+        std::to_string(y) + " " +
+        std::to_string(i) + "," +
+        std::to_string(j) + "\n";
+}
+
+std::string check_if_threathens_other_queen(int x, int y, bool board[8][8]) {
+    std::string collisions = "";
     for (int i = x + 1; i < 8; i++) {
-        if (board[i][y]) return true;
+        if (board[i][y])
+            collisions += format_collision(x, y, i, y);
     }
 
     for (int j = y + 1; j < 8; j++) {
-        if (board[x][j]) return true;
+        if (board[x][j])
+            collisions += format_collision(x, y, x, j);
     }
 
     for (int i = x + 1, j = y + 1; i < 8 && j < 8; i++, j++) {
-        if (board[i][j]) return true;
+        if (board[i][j])
+            collisions += format_collision(x, y, i, j);
     }
 
     for (int i = x + 1, j = y - 1; i < 8 && j >= 0; i++, j--) {
-        if (board[i][j]) return true;
+        if (board[i][j])
+            collisions += format_collision(x, y, i, j);
     }
 
-    return false;
+    return collisions;
 }
 
 int is_valid_eight_queens(std::string file_name) {
@@ -38,6 +50,8 @@ int is_valid_eight_queens(std::string file_name) {
     bool board[8][8] = {false};
     int row_count = 0;
     int queen_count = 0;
+    std::string attacks = "";
+
     file.open(file_name);
 
     if (file.is_open()) {
@@ -71,9 +85,18 @@ int is_valid_eight_queens(std::string file_name) {
 
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
-            if (board[i][j] && check_if_threathens_other_queen(i, j, board))
-                return 0;
+            std::string currenct_attacks =
+                check_if_threathens_other_queen(i, j, board);
+            if (board[i][j] && currenct_attacks.length() > 0)
+                attacks += currenct_attacks;
         }
+    }
+
+    if (attacks.length() > 0) {
+        std::ofstream attacks_file ("attacks.txt");
+        attacks_file << attacks;
+        attacks_file.close();
+        return 0;
     }
 
     return 1;
